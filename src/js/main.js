318090c1e35e411afbdad24c46d3037627d71d32
Lareta064 <lareta064@gmail.com>
2024-08-14 09:07:01 +0300

    fix-body-animate

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function (){
 	$('.lazy').lazy();
 	const bodyEl = document.body;
-	
+	const bodyAnimate = document.querySelector('.body-animate');
+	if(bodyAnimate)
+		bodyAnimate.classList.add("fade-in");
 	/*====cooki popup remove=====*/
 	const cookiPopup = document.querySelector('.cooki-popup');
 	if(cookiPopup){
